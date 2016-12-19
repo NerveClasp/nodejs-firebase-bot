5ca4a480a53a4710368b03ba86e16a6b6d41de85
NerveClasp <nerveclasp@gmail.com>
2016-12-19 15:21:37 +0200

    Implemented adition of new data to Firebase database. Beardy Tutorials Ep. 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Twitter = require('twitter');
 const config = require('./config.json');
+const admin = require("firebase-admin");
 
 const client = new Twitter({
   consumer_key: config.twitter.consumer_key,
@@ -8,10 +9,23 @@ const client = new Twitter({
   access_token_secret: config.twitter.access_token_secret
 });
 
+admin.initializeApp({
+  credential: admin.credential.cert(config.admin),
+  databaseURL: "https://nodejs-firebase-bot.firebaseio.com"
+});
+const db = admin.database();
+const ref = db.ref("/added");
+let firstEntry = {
+  text: "Second Tweet! How you doin'?)\nGoing Good! Recording our second video",
+  id: 0,
+  id_str: ""
+};
+ref.push(firstEntry);
+/*
 client.post('statuses/update',
     {status: 'First Tweet! Yeah! Cmon!\nHello, fellas!'},
     function(error, tweet, response) {
   if(error) throw error;
   console.log(tweet);  // Tweet body.
   // console.log(response);  // Raw response object.
-});
+});*/
