52978a5791723d3ff8dbd573e07f12254eaa1211
NerveClasp <nerveclasp@gmail.com>
2016-12-19 15:54:15 +0200

    Tweeted from the Firebase database! Beardy Tutorials Ep. 3

--- index.js
+++ index.js
@@ -20,12 +20,28 @@ let firstEntry = {
   id: 0,
   id_str: ""
 };
-ref.push(firstEntry);
-/*
-client.post('statuses/update',
-    {status: 'First Tweet! Yeah! Cmon!\nHello, fellas!'},
-    function(error, tweet, response) {
-  if(error) throw error;
-  console.log(tweet);  // Tweet body.
-  // console.log(response);  // Raw response object.
-});*/
+// ref.push(firstEntry);
+ref.on("value", function(snapshot) {
+  let val = snapshot.val();
+  let key = "-KZMAotA5O3MLkGbbNJj";
+  // console.log(val);
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
+
+ref.child("-KZMAotA5O3MLkGbbNJj").on('value', function (snap) {
+  let val = snap.val();
+  postTweet(val.text);
+  // console.log(val.text);
+});
+
+let postTweet = function(text){
+  client.post('statuses/update',
+      {status: text},
+      function(error, tweet, response) {
+    if(error) throw error;
+    console.log("Tweeted!", text);
+    console.log(tweet);  // Tweet body.
+    // console.log(response);  // Raw response object.
+  });
+}
